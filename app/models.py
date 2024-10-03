35b29b837ed9aa30b7b0284aab3c6adacdfc360c
karastoyanov on vivobook <karastoqnov.alexandar@gmail.com>
2024-10-03 22:29:19 +0300

    Boss and Challenge relationships added

--- app/models.py
+++ app/models.py
@@ -1,6 +1,8 @@
 import random
+from datetime import datetime
 from app.database_conn import db
 from sqlalchemy.orm import relationship
+from sqlalchemy import Enum
 
 
 # Underworld Boss Class
@@ -13,6 +15,9 @@ class Boss(db.Model):
     boss_difficulty = db.Column(db.String(50), nullable=False)
     boss_specialty = db.Column(db.String(50), nullable=False)
     boss_description = db.Column(db.Text, nullable=False)
+    
+    # Relationship with Challenge Class
+    challenges = relationship('Challenge', back_populates='boss_name')
     
     # Class Constructor
     def __init__(self, boss_name, boss_title, boss_language, boss_difficulty, boss_specialty, boss_description):
@@ -46,3 +51,38 @@ class Boss(db.Model):
             db.session.commit()
         else:
             print(f"Boss with ID {boss_id} not found.")
+            
+            
+# Underworld Challenge Class
+class Challenge(db.Model):
+    __tablename__ = 'underworld_challenges'
+    challenge_id = db.Column(db.String(11), primary_key=True, nullable=False)
+    boss_id = db.Column(db.String(11), db.ForeignKey('underworld_bosses.boss_id'), nullable=False)
+    # Relationship with Boss Class
+    boss_name = relationship("Boss", back_populates="challenges")
+    user_id = db.Column(db.String(11), nullable=False)
+    user_name = db.Column(db.String(50), nullable=False)
+    challenge_date = db.Column(db.DateTime, default=datetime.now())
+    state = db.Column(Enum('In Progress', 'Finished', name='challenge_state'), nullable=False)
+    
+    # Class Constructor
+    def __init__(self, challenge_id, boss_id, boss_name, user_id, user_name, state):
+        self.challenge_id = challenge_id
+        self.boss_id = boss_id
+        self.boss_name = boss_name
+        self.user_id = user_id
+        self.user_name = user_name
+        self.state = state
+    
+    # Generate Challenge ID
+    def generate_challenge_id(self):
+        # Generate a unique challenge ID in the format CHALLENGE-XXXXXX
+        while True:
+            challenge_id = f'CHALLENGE-{random.randint(100000, 999999)}'
+            if not Challenge.query.filter_by(challenge_id=challenge_id).first():  # Ensure uniqueness
+                return challenge_id
+    
+    # Save the challenge to the database
+    def create_challenge(self):
+        db.session.add(self)
+        db.session.commit()
