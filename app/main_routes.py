d317f44501cd826ceebe0cce58c1c13e989d4780
ragnarok dev-station <karastoqnov.alexandar@gmail.com>
2024-10-31 21:55:50 +0200

    allowed ips updated

--- app/main_routes.py
+++ app/main_routes.py
@@ -7,10 +7,10 @@ bp = Blueprint('main', __name__)
 # Check the status of the Underworld Realm
 @bp.route('/', methods=['GET'])
 def underworld_status():
-    return jsonify({"message": "Underworld Realm is running!"}), 200
-    # allowed_ips = os.getenv('ALLOWED_IPs', '').split(',')
-    # if request.remote_addr not in allowed_ips:
-    #     return jsonify({"error": "Unauthorized access"}), 403
     # return jsonify({"message": "Underworld Realm is running!"}), 200
+    allowed_ips = os.getenv('ALLOWED_IPs', '').split(',')
+    if request.remote_addr not in allowed_ips:
+        return jsonify({"error": "Unauthorized access"}), 403
+    return jsonify({"message": "Underworld Realm is running!"}), 200
 
 
