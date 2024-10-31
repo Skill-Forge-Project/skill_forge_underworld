ae4d4c009c055a55661661f3b133f8016eef8ca0
ragnarok dev-station <karastoqnov.alexandar@gmail.com>
2024-10-31 22:08:24 +0200

    before_app_request global route implemented

--- app/main_routes.py
+++ app/main_routes.py
@@ -4,13 +4,16 @@ from flask import Blueprint, request, jsonify
 # Create a Blueprint for the main routes
 bp = Blueprint('main', __name__)
 
-# Check the status of the Underworld Realm
-@bp.route('/', methods=['GET'])
-def underworld_status():
-    # return jsonify({"message": "Underworld Realm is running!"}), 200
+@bp.before_app_request
+def check_underworld_status():
+    # List of allowed IPs from environment variable
     allowed_ips = os.getenv('ALLOWED_IPs', '').split(',')
+    # Check if IP is authorized
     if request.remote_addr not in allowed_ips:
         return jsonify({"error": "Unauthorized access"}), 403
-    return jsonify({"message": "Underworld Realm is running!"}), 200
 
+# Define the Underworld status route
+@bp.route('/', methods=['GET'])
+def underworld_status():
+    return jsonify({"message": "Underworld Realm is running!"}), 200
 
